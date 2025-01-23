18cc2dbf08902b5186accb4e041ad757f2c4329b
vignesh4769 <siddhugarikina4769@gmail.com>
2025-01-23 21:39:54 +0530

    Updated App component and added Login, SignUp, and ForgotPassword components

---
+++ frontend/src/components/ForgotPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ForgotPassword() {
+  return (
+    <div>
+      <h1>ahh.. here you are again ,forgetting the password</h1>
+    </div>
+  )
+}
+
+export default ForgotPassword
